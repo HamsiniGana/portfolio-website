197821e4e343057ae1a5115c9449806f1d673d62
Hamsini Ganesan <hamsinig123@gmail.com>
2025-02-12 22:06:41 +1100

    Adding the homepage

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,11 @@
+import worldImage from './images/World-globe-clip-art-clipart-2-wikiclipart-wikiclipart.png'
 function App() {
   return (
     <div className="App">
       <header>
         <title>My portfolio</title>
       </header>
+
       <nav>
         <ul>
           <li>Home</li>
@@ -11,9 +13,19 @@ function App() {
           <li>Hobbies</li>
         </ul>
       </nav>
-      <section className='Homepage'>
-    
+
+      <h4></h4>
+
+      <section className="homepage">
+        <h1 className="HomepageElement heading">
+          Welcome to my world!
+        </h1>
+        <img className="HomepageElement worldImage" src={worldImage} alt='world image'></img>
+
+        <h3 className="HomepageElement passportText"> Click here to get a visual passport to tour my world!</h3>
+        
       </section>
+
       <footer>
       </footer>
     </div>
